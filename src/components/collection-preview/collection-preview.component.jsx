d79dc8d81c8a8e5617bf77b44a7cce38dc9d3cdd
Tzvety <tzvety.hr@gmail.com>
2020-07-06 18:49:14 +0100

    Moved directory and shop data into its own reducer, created respective selectors and updated the components to use the new redux data

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import { Link, withRouter } from 'react-router-dom';
 import CollectionItem from '../collection-item/collection-item.component';
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items }) => (
+const CollectionPreview = ({ title, items, routeName, match }) => (
   <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
+    <Link to={`${match.url}/${routeName}`}>
+      <h1 className="title">{title.toUpperCase()}</h1>
+    </Link>
+
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
@@ -16,4 +19,4 @@ const CollectionPreview = ({ title, items }) => (
   </div>
 );
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,8 +1,23 @@
 import { combineReducers } from 'redux';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import shopReducer from './shop/shop.reducer';
+import directoryReducer from './directory/directory.reducer';
 
-export default combineReducers({
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  shop: shopReducer,
+  directory: directoryReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
